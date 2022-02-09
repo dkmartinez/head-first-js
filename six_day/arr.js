2514ed17a8684c6e60cb3d75fa03f513096a8c7e
dianalcamposm <dianacampos2112@gmail.com>
2022-02-08 19:27:41 -0600

    "my array"

---
+++ six_day/arr.js
@@ -0,0 +1,21 @@
+//array literal, because we'are literally writing out what goesin the array
+var genres = ["80s", "90s", "Electronic", "Folk"];
+
+
+//but you can aso omit the initial items and just create an empty array
+var genres = [];
+
+//and to add new values to an empty array
+genres[0] ="Rockability";
+genres[1] ="Ambient";
+
+//other way to add new items using push
+//create a new item in the index, and set the value
+//pushing a new value on the end of my array
+genres.push("Rockability");
+genres.push("Ambien"); 
+
+
+//other way to create an array
+
+var myArr = new Array[3]; //length 3
