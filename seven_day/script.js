9da9b808548a29a1b99c10a93dd1d862d9c17ab2
dianalcamposm <dianacampos2112@gmail.com>
2022-02-09 20:09:07 -0600

    "challenge sort by"

--- seven_day/script.js
+++ seven_day/script.js
@@ -15,17 +15,37 @@ console.log(number);
 // //create a function than create an array with random numbers
 
 function random(value) {
-  var number = [];
+    //create an empty array to save the random numbers
+  var number= [];
   var i = 0;
   while (i < value) {
     i++;
 
     var aleatorio = Math.floor(Math.random() * 50);
     number.push(aleatorio);
+
   }
   return number;
 }
 
 console.log(random(3));
-console.log(random(5));
+console.log(random(6));
 console.log(random(10));
+
+
+//order the number
+var myValue = [45,22,33,11,22,20,12]
+var i;
+var j;
+
+    for(i=0; i<myValue[i]; i++){
+     var theNumber = myValue[i];
+    console.log(theNumber) ; 
+
+   for ( j=0; j<theNumber; j++){
+    myValue[i];
+    if(theNumber > j){
+        console.log(theNumber + " order");
+    }
+}
+}
